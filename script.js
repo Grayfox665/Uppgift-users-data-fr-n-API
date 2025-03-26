fc54b80c5a95b7ad520f8a00ca8526123bbca191
Erik Eriksson Broomé <erik.o.eriksson@telia.com>
2025-03-26 18:50:31 +0100

    chaged the fetch data script to is async and await

--- script.js
+++ script.js
@@ -1,37 +1,41 @@
-function getAllUsers(){
 
-    fetch('https://jsonplaceholder.typicode.com/users')
-    .then(response =>{
+async function getAllUsers(){
+
+    try{
+        const response = await fetch("https://jsonplaceholder.typicode.com/users");
         if(!response.ok){
-            throw new Error("Network was not ok")
+            throw new Error("Fel vid hämtning av data : ")
         }
-        return response.json();
-    })
-    .then(users => {
-        let usersList = document.getElementById("users-container")
-
-        users.forEach(user => {
-            let div = document.createElement("div");
-            let h3Name = document.createElement("h3");
-            let h3Username = document.createElement("h3");
-            let h3Email = document.createElement("h3");
-
-            h3Name.textContent = user.name;
-            h3Username.textContent = user.username;
-            h3Email.textContent = user.email;
-
-            usersList.appendChild(div);
-            div.setAttribute("class","user");
-
-            div.append(h3Name, h3Username, h3Email);
-            
-
-
-        });
-    })
-    .catch(e => {
-        alert("Error : " + e);
-    })
+        const usersData = await response.json();
+        console.log(usersData);
+        return usersData;
+    }
+    catch(error){
+        console.log(error);
+    }
 }
 
-getAllUsers();
+async function displayAllUsers(){
+    const display = await getAllUsers()
+
+    const usersList = document.getElementById("users-container");
+
+    display.forEach(user => {
+        let div = document.createElement("div");
+                    let h3Name = document.createElement("h3");
+                    let h3Username = document.createElement("h3");
+                    let h3Email = document.createElement("h3");
+        
+                    h3Name.textContent ="Name : " + user.name;
+                    h3Username.textContent ="Username : " + user.username;
+                    h3Email.textContent ="Email : " + user.email;
+        
+                    usersList.appendChild(div);
+                    div.setAttribute("class","user");
+        
+                    div.append(h3Name, h3Username, h3Email);        
+    });
+}
+
+
+displayAllUsers();
