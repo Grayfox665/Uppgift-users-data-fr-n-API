bf1b9e380e95621de8407c58d719fd310338d2b8
Erik Eriksson Broomé <erik.o.eriksson@telia.com>
2025-04-02 19:49:02 +0200

    Changed from id:ing the users with id to index order. changed the toggle function to work with this new id.

--- JS/fetchUserData.js
+++ JS/fetchUserData.js
@@ -1,6 +1,7 @@
 
 async function getAllUsers(){
 
+
     try{
         const response = await fetch("https://jsonplaceholder.typicode.com/users");
         if(!response.ok){
@@ -16,11 +17,15 @@ async function getAllUsers(){
 }
 
 async function displayAllUsers(){
+    //awaits the result and gets the User Array data from getAllUsers function
     const display = await getAllUsers()
+
+
 
     const usersList = document.getElementById("users-container");
 
     display.forEach(user => {
+
         /*Article for each user */
         let article = document.createElement("article");
         
@@ -34,7 +39,7 @@ async function displayAllUsers(){
             let h3Phone = document.createElement("h3");
             let h3Company = document.createElement("h3");
 
-
+        
             /* inserts the info to the h3 for each article */
             h3Name.textContent ="Name : " + user.name;
             h3Username.textContent ="Username : " + user.username;
@@ -45,23 +50,23 @@ async function displayAllUsers(){
             h3Company.textContent ="Company Name : " + user.company.name;
         
             usersList.appendChild(article);
-            article.setAttribute("class",`user ${user.id}`);
+            article.setAttribute("class",`user ${display.indexOf(user)}`);
 
             /*the div with extra info */
             const sectionInfo = document.createElement("section");
             sectionInfo.classList.add('more-info');
-            sectionInfo.setAttribute("id",`info-${user.id}`);
+            sectionInfo.setAttribute("id",`info-${display.indexOf(user)}`);
             sectionInfo.append(h3City,h3Phone,h3Company);
 
             /*the toggle button */
             const infoButton = document.createElement("button");
             infoButton.textContent = "More Info";
-            infoButton.addEventListener('click', () => toggleInfo(user.id));
+            infoButton.addEventListener('click', () => toggleInfo(display.indexOf(user)));
 
             /*the extra info */
-            h3City.setAttribute("class",`info ${user.id}`);
-            h3Phone.setAttribute("class",`info ${user.id}`);
-            h3Company.setAttribute("class",`info ${user.id}`);
+            h3City.setAttribute("class",`info ${display.indexOf(user)}`);
+            h3Phone.setAttribute("class",`info ${display.indexOf(user)}`);
+            h3Company.setAttribute("class",`info ${display.indexOf(user)}`);
         
             article.append(h3Name, h3Username, h3Email,infoButton, sectionInfo);  
 
--- JS/toggleInfo.js
+++ JS/toggleInfo.js
@@ -1,7 +1,7 @@
 
 /*activates the showing of extra info for user when corresponding button is pressed */
-function toggleInfo(userId){
-    const sectionInfo = document.getElementById(`info-${userId}`);
+function toggleInfo(userIndex){
+    const sectionInfo = document.getElementById(`info-${userIndex}`);
     if (sectionInfo.style.display === 'none' || sectionInfo.style.display === ''){
         sectionInfo.style.display = "block";
     } else {
